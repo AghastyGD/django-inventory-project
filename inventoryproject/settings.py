707daab9c2c35dc73b03a71b6c9a01a65b4978c6
AghastyGD <augustodomingosvasco@gmail.com>
2023-10-03 16:16:12 +0200

    configuracao de ambientes virtuais

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,12 +137,10 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'dashboard-index'
 LOGIN_URL = 'user-login'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aghastygdproductions10@gmail.com'
-EMAIL_HOST_PASSWORD = 'nwlxftjzdqxjlgcm'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', default='587')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
