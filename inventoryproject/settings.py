e225cf6f6625549adc740a0774581955bda05f12
Augusto Domingos <augustodomingosvasco@gmail.com>
2024-07-02 21:23:47 +0200

    config static files

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -130,11 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = (BASE_DIR/"asert/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "asert", "static")
 
 MEDIA_ROOT = (BASE_DIR/'media')
 
