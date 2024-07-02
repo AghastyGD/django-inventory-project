60f66e85fa6907859b22f6cf69e7cc8546f2380c
Augusto Domingos <augustodomingosvasco@gmail.com>
2024-07-02 16:29:40 +0200

    scripts to deploy

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'inventoryproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
